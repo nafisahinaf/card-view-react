3657fb516b1c1611010bbc3dd193e5d375b5c055
Nafisah <nafisahkarimah@mail.ugm.ac.id>
2020-03-26 22:05:32 +0700

    Updates

---
+++ precache-manifest.3e3f21f2a4a25bfae89192464e5bd198.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d4bcafc9fc5d37a12a48c28d7c03a1",
+    "url": "/WEB3B-15707-NAFISAH-REACT/index.html"
+  },
+  {
+    "revision": "24d5c5480a46a516d391",
+    "url": "/WEB3B-15707-NAFISAH-REACT/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "0f6f807308285c9a6c16",
+    "url": "/WEB3B-15707-NAFISAH-REACT/static/css/main.36853ddf.chunk.css"
+  },
+  {
+    "revision": "24d5c5480a46a516d391",
+    "url": "/WEB3B-15707-NAFISAH-REACT/static/js/2.34fa34fe.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/WEB3B-15707-NAFISAH-REACT/static/js/2.34fa34fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f6f807308285c9a6c16",
+    "url": "/WEB3B-15707-NAFISAH-REACT/static/js/main.0e9fad9b.chunk.js"
+  },
+  {
+    "revision": "3db0fbd03416ba80a8a4",
+    "url": "/WEB3B-15707-NAFISAH-REACT/static/js/runtime-main.dcfd6445.js"
+  },
+  {
+    "revision": "9e26156101f9d3e280dcd7265ad6c997",
+    "url": "/WEB3B-15707-NAFISAH-REACT/static/media/1.9e261561.jpg"
+  },
+  {
+    "revision": "81567f0baec9e779a31360f8fc268955",
+    "url": "/WEB3B-15707-NAFISAH-REACT/static/media/2.81567f0b.jpg"
+  }
+]);
